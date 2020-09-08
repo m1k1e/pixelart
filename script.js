e52f50a014362bb59ca408d2ca2c4379645ecf42
m1k1e <mikle.fedorenko@gmail.com>
2020-09-08 21:26:56 +0300

    Add work script for draw pixels and erease

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-*/
+/*
 
 1. Все «пиксели» имеют класс pixel.
 2. Выпадающий список с цветами имеет класс chosen-color.
@@ -8,3 +8,22 @@
 6. Если в момент клика ластик включён, фон нажатого «пикселя» должен стать белым — 'white'.
 
 */
+
+let colorSetting = document.querySelector('.chosen-color');
+let eraser = document.querySelector('.eraser');
+let pixels = document.querySelectorAll('.pixel');
+
+
+
+for(let pixel of pixels){
+	pixel.onclick = function() {
+	if(eraser.checked){
+		pixel.style.backgroundColor ='white';
+	}else {
+		pixel.style.backgroundColor = colorSetting.value;
+	 }
+	
+	};
+}
+
+
